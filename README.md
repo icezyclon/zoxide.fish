a6dabb16dd2de570a2005617ea4510f0beb6dc53
Felix Wallner <felix.wallner@student.tugraz.at>
2022-06-18 17:46:22 +0200

    do not source zoxide init, provide functions from zoxide 0.8.1 instead

--- README.md
+++ README.md
@@ -3,20 +3,14 @@
 > Tab completion and initialization for [zoxide](https://github.com/ajeetdsouza/zoxide) in [fish shell](https://fishshell.com/).
 
 [Zoxide](https://github.com/ajeetdsouza/zoxide) is a *smarter cd command* and can be used as a *direct replacement* for cd in the shell.
-Once sourced by this plugin it provides the **z** command to navigate between directories.
-**z** may be used as a *drop in replacement* for cd.
+Once sourced by this plugin it provides the **z** command to navigate between directories, which also replaces the default cd command.
 
 By default, tab completion for **z** does *first* complete directories and *then* zoxide queries, if no directories match the given string.
 
 **zi** may be used for interactive selection of recently visited directories.
 
-After installation cd may be used as an abbreviation for **z** like so:
-
-```fish
-abbr cd z
-```
-
-This leaves the normal cd command unchanged for scripts but replaced cd with z during normal interactive shell usage.
+After installation the cd command will be aliased to **z** by default.
+> In case this is not desired `_zoxide_uninstall` may be called in `config.fish` after the plugin was initialized to restore the old cd and keep **z** as a separate function.
 
 ## Requirements
 
@@ -28,9 +22,12 @@ On Ubuntu and other Debian systems, **zoxide** may be installed with apt:
 sudo apt install zoxide
 ```
 
-> This plugin was written for [fish version 3.5.0](https://fishshell.com/docs/3.5/), there is no guarantee that it will work for older versions of fish.
+## Versions
 
-> The plugin sources `zoxide init fish`, which was tested for the apt version `0.4.3-2` only.
+Because [zoxide](https://github.com/ajeetdsouza/zoxide) changes the the init script based on the version, and especially because the apt version of **zoxide** is still very old, this plugin does *not* source `zoxide init fish`.
+It instead provides modified copies of the required functions from **zoxide** version 0.8.1 with custom tab completion.
+
+> This plugin was written for [fish version 3.5.0](https://fishshell.com/docs/3.5/), there is no guarantee that it will work for older versions of fish.
 
 ## Installation
 
@@ -44,7 +41,7 @@ The default behavior for tab completion may change.
 To get the same stable version of the behavior install from a certain tag instead, e.g.
 
 ```console
-fisher install icezyclon/zoxide.fish@1.0
+fisher install icezyclon/zoxide.fish@2.0
 ```
 
 ## Related Plugins
--- conf.d/zoxide.fish
+++ conf.d/zoxide.fish
@@ -1,33 +1,65 @@
-set -g _zoxide_msg_promt (string replace " " "" "[$(set_color brcyan)plugin$(set_color normal):$(set_color green)zoxide$(set_color normal)]")
-
 if status is-interactive
 
     if type -q zoxide
-        zoxide init fish | source
 
+        # -------------
+        # 'zoxide init fish' is very different for different versions of zoxide
+        # to guarantee the same behavior we define these functions ourself,
+        # especially because the apt package is so old
+        # most of these functions were taken from https://github.com/ajeetdsouza/zoxide
+        # from version 0.8.1
+
+        if ! builtin functions -q _zoxide_cd
+            if builtin functions -q cd
+                builtin functions -c cd _zoxide_cd
+            else
+                alias _zoxide_cd='builtin cd'
+            end
+        end
+
+        function _zoxide_hook --on-variable PWD
+            test -z "$fish_private_mode"
+            and command zoxide add -- (builtin pwd -L)
+        end
+
+        function z
+            set argc (count $argv)
+            if test $argc -eq 0
+                _zoxide_cd $HOME
+            else if test "$argv" = -
+                _zoxide_cd -
+            else if test $argc -eq 1 -a -d $argv[1]
+                _zoxide_cd $argv[1]
+            else
+                set -l result (command zoxide query --exclude (builtin pwd -L) -- $argv)
+                and _zoxide_cd $result
+            end
+        end
+
+        function zi
+            set -l result (command zoxide query -i -- $argv)
+            and _zoxide_cd $result
+        end
+
+        # -------------
+
+        alias cd=z
+
+        # use custom completion
         complete -c z -f
         complete -c z -n __fish_use_subcommand -x -a '(_zoxide_z_complete)'
     else
-        echo $_zoxide_msg_promt "Command 'zoxide' cannot be found. Not installed or not in path"
+        echo "[plugin: zoxide] Command 'zoxide' cannot be found. Not installed or not in path"
     end
 
-end
-
-function _zoxide_install --on-event zoxide_install
-    if status is-interactive
-        switch (read --prompt-str="$_zoxide_msg_promt Abbreviate cd to z? [Y/n] " | string lower)
-            case y ye yes ''
-                echo "$_zoxide_msg_promt Adding" (echo -ns "abbr cd z" | fish_indent --ansi)
-                abbr -a -U cd z
-            case '*'
-                echo "$_zoxide_msg_promt Note: Type" (echo -ns "abbr cd z" | fish_indent --ansi) "to abbreviate cd to z"
-        end
-    end
 end
 
 function _zoxide_uninstall --on-event zoxide_uninstall
-    if abbr -q cd && abbr -s | grep "\-\- cd z" >/dev/null
-        abbr -e cd
-        echo "$_zoxide_msg_promt Removing" (echo -ns "abbr cd z" | fish_indent --ansi)
+    if alias | grep "alias cd z" >/dev/null
+        functions -e cd
+    end
+    if builtin functions -q _zoxide_cd && not functions -q cd
+        # restore old cd
+        builtin functions -c _zoxide_cd cd
     end
 end
